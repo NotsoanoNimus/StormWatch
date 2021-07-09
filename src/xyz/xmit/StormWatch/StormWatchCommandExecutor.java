3c0c0f227c63a285a8cac7cfdd7f833b0c77a1b0
Notsoano Nimus <github@xmit.xyz>
2021-07-08 21:35:53 -0400

    Making space for future CommandExecutor calls that do not follow the action-storm-value sequence and thus can do their own parameter verifications in their respective methods. Split out the non-persistent toggle and the config toggle for clarity's (and hopefully permissions') sake later on down the line.

--- src/xyz/xmit/StormWatch/StormWatchCommandExecutor.java
+++ src/xyz/xmit/StormWatch/StormWatchCommandExecutor.java
@@ -5,9 +5,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Locale;
+import java.util.*;
+
+import static org.bukkit.craftbukkit.libs.org.apache.http.client.methods.RequestBuilder.put;
 
 /**
  * Command executor for the primary StormWatch command <code>/stormgr</code>. This keeps all the
@@ -42,47 +42,69 @@ public class StormWatchCommandExecutor implements CommandExecutor {
      */
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // All stormgr commands require at least four parameters. Here are some different permutations:
+        // Most stormgr commands require at least three parameters (action storm value).
+        // Here are some different permutations of that:
         //   /stormgr cast impact PlayerGuy [impact options]
         //   /stormgr toggle impact enabled
-        String senderName = (sender instanceof Player) ? ((Player)sender).getDisplayName() : sender.getName();
-        System.out.println("SENDER: " + senderName + "\nCOMMAND: " + command
-                + "\nLABEL: " + label + "\nARGS: " + String.join(", ", args));
+        // Some that may not include "query" type commands for getting information from the plugin.
+        var nonStandardCmds = new ArrayList<String>() {{
+            put("query");
+        }};
+
+        // Capture the command sender.
         this.whoSent = sender;
         // Firstly, refresh the map of types -> class-paths.
         this.refreshStormTypes();
-        if(args.length < 3) {
-            sender.sendMessage("Invalid parameters.");
-            return false;
-        } else if(!this.typesToClassPaths.containsKey(args[1])) {
-            sender.sendMessage("Invalid Storm type: " + args[1]);
-            return false;
-        }
+        // Get whether this is a "query" command or some other command that doesn't
+        //   require the first 3 typical params: (action storm value)
+        boolean followsPattern = !(nonStandardCmds.contains(args[0].toLowerCase(Locale.ROOT)));
 
-        // Validate that the selected storm type is valid, and instantiate a Storm object corresponding to it.
-        Class<? extends Storm> chosenStorm;
-        Storm newStorm;
-        try {
-            chosenStorm = (Class<? extends Storm>)Class.forName(  this.typesToClassPaths.get( args[1] )  );
-            if(!StormWatch.getStormManager().getRegisteredStormTypes().contains(chosenStorm)) {
-                throw new Exception(chosenStorm.toString() + ", is not a registered Storm type!");
-            }
-            newStorm = (Storm)chosenStorm.cast(chosenStorm.getDeclaredConstructor().newInstance());
-            newStorm.setIsCalledByCommand();
-        } catch (Exception ex) {
-            sender.sendMessage("That is not a currently-registered Storm type.");
-            StormWatch.log(ex);
-            return false;
-        }
-
-        switch(args[0].toLowerCase(Locale.ROOT)) {
-            case "cast":
-                return this.castCommand(newStorm, args[2], Arrays.copyOfRange(args, 3, args.length - 1));
-            case "toggle":
-                return this.toggleStorm(chosenStorm, args[1], args[2]);
-            default:
-                sender.sendMessage("Invalid parameter: " + args[0]);
+        // Parse some of the parameters, if they follow the 3 param pattern.
+        if(followsPattern) {
+            if (args.length < 3) {
+                sender.sendMessage("Invalid parameters.");
                 return false;
+            } else if (!this.typesToClassPaths.containsKey(args[1])) {
+                sender.sendMessage("Invalid Storm type: " + args[1]);
+                return false;
+            }
+
+            // Validate that the selected storm type is valid, and instantiate a Storm object corresponding to it.
+            Class<? extends Storm> chosenStorm;
+            Storm newStorm;
+            try {
+                chosenStorm = (Class<? extends Storm>) Class.forName(this.typesToClassPaths.get(args[1]));
+                if (!StormWatch.getStormManager().getRegisteredStormTypes().contains(chosenStorm)) {
+                    throw new Exception(chosenStorm.toString() + ", is not a registered Storm type!");
+                }
+                newStorm = (Storm) chosenStorm.cast(chosenStorm.getDeclaredConstructor().newInstance());
+                newStorm.setIsCalledByCommand();
+            } catch (Exception ex) {
+                sender.sendMessage("That is not a currently-registered Storm type.");
+                StormWatch.log(ex);
+                return false;
+            }
+
+            switch (args[0].toLowerCase(Locale.ROOT)) {
+                case "cast":
+                    return this.castCommand(newStorm, args[2], Arrays.copyOfRange(args, 3, args.length - 1));
+                case "toggle":
+                    return this.toggleStorm(chosenStorm, args[1], args[2]);
+                case "configtoggle":
+                    return this.configToggleStorm(chosenStorm, args[1], args[2]);
+                default:
+                    sender.sendMessage("Invalid parameter: " + args[0]);
+                    return false;
+            }
+        } else {
+            // Don't verify anything. Let the method calls do that.
+            switch (args[0].toLowerCase(Locale.ROOT)) {
+                case "query":
+                    return this.queryCommand(Arrays.copyOfRange(args, 1, args.length - 1));
+                default:
+                    sender.sendMessage("Invalid parameter: " + args[0]);
+                    return false;
+            }
         }
     }
 
@@ -104,45 +126,75 @@ public class StormWatchCommandExecutor implements CommandExecutor {
         return true;
     }
 
+
+    // TODO: This mess can probably be condensed like its shinier cousin below.
     private boolean toggleStorm(Class<? extends Storm> storm, String stormName, String enOrDisable) {
         var mgr = StormWatch.getStormManager();
         switch (enOrDisable.toLowerCase(Locale.ROOT)) {
             case "enable" -> {
-                // First, make sure the Storm type is registered.
+                this.whoSent.sendMessage("Attempting to register Storm type: " + storm.getName());
                 if(!mgr.getRegisteredStormTypes().contains(storm)) {
-                    this.whoSent.sendMessage("Attempting to register Storm type: " + storm.getName());
                     if(!mgr.registerNewStormType(storm)) {
                         this.whoSent.sendMessage("Failed to register the requested Storm type.");
                         return false;
+                    } else {
+                        this.whoSent.sendMessage("Storm type registered successfully.");
+                        return true;
                     }
+                } else {
+                    this.whoSent.sendMessage("Storm type '" + storm.getName() + "' is already registered.");
+                    return false;
                 }
-                // Now, config-enable the storm.
-                return StormWatch.getStormConfig().setConfigValue(
-                        stormName,
-                        Storm.RequiredConfigurationKeyNames.ENABLED,
-                        true
-                );
             }
             case "disable" -> {
+                this.whoSent.sendMessage("Attempting to unregister Storm type: " + storm.getName());
                 if(mgr.getRegisteredStormTypes().contains(storm)) {
-                    this.whoSent.sendMessage("Attempting to unregister Storm type: " + storm.getName());
                     if(!mgr.unregisterStormType(storm)) {
                         this.whoSent.sendMessage("Failed to unregister that Storm type!");
                         return false;
+                    } else {
+                        this.whoSent.sendMessage("Storm type unregistered successfully.");
+                        return true;
                     }
+                } else {
+                    // I don't _think_ this should ever really happen, but ehh.
+                    this.whoSent.sendMessage("Storm type '" + storm.getName() + "' is not registered.");
+                    return false;
                 }
-                // Now, config-disable the storm.
-                return StormWatch.getStormConfig().setConfigValue(
-                        stormName,
-                        Storm.RequiredConfigurationKeyNames.ENABLED,
-                        false
-                );
             }
             default -> {
-                this.whoSent.sendMessage("The provided parameter '" + enOrDisable
-                        + "' is invalid. Must be 'enable' or 'disable'.");
+                this.whoSent.sendMessage("The parameter '" + enOrDisable
+                        + "' is not valid. Must be 'enable' or 'disable'.");
                 return false;
             }
         }
+    }
+
+    private boolean configToggleStorm(Class<? extends Storm> storm, String stormName, String enOrDisable) {
+        boolean setEnabled;
+        switch(enOrDisable.toLowerCase(Locale.ROOT)) {
+            case "enable" -> { setEnabled = true; }
+            case "disable" -> { setEnabled = false; }
+            default -> {
+                this.whoSent.sendMessage("The parameter '" + enOrDisable
+                        + "' is not valid. Must be 'enable' or 'disable'.");
+                return false;
+            }
+        }
+        boolean successfulChange = false;
+        try {
+            successfulChange = StormWatch.getStormConfig().setConfigValue(
+                stormName, Storm.RequiredConfigurationKeyNames.ENABLED, setEnabled
+            );
+        } catch(Exception ex) { StormWatch.log(ex); }
+        this.whoSent.sendMessage((successfulChange ? "Successfully" : "Failed to") + " config-" +
+                (setEnabled ? "en" : "dis") + "able the '" + stormName + "' type.");
+        return successfulChange;
+    }
+
+
+    // TODO: Implement
+    private boolean queryCommand(String[] queryParams) {
+        return true;
     }
 }
